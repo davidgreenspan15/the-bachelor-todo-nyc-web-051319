2eb879f523cbb52b13f23051b3ee29713ff2cd41
David Greenspan <ramigreenspan@gmail.com>
2019-05-08 08:36:28 -0400

    bachelor

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,69 @@
+require 'pry'
+winner_name = ""
 def get_first_name_of_season_winner(data, season)
-  # code here
+data[season].each do |contestant|
+  contestant.each do |status, info|
+if info == "Winner"
+  winner_name = contestant["name"].split(" ").first
+return winner_name
+      end
+    end
+  end
+
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, array|
+    array.each do |contestant|
+      contestant.each do |status,info|
+        if info == occupation
+          return contestant["name"]
+        end
+      end
+    end
+  end
 end
+
+
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
-end
+counter = 0
+  data.each do |season, array|
+    array.each do |contestant|
+      contestant.each do |status,info|
+        if info == hometown
+          counter +=1
+        end
+      end
+    end
+  end
+  counter
+  end
+
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, array|
+    array.each do |contestant|
+      contestant.each do |status,info|
+        if info == hometown
+          return contestant["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+total_ages = 0
+counter = 0
+  data[season].each do |contestant|
+      contestant.each do |status,info|
+        if status == "age"
+          counter +=1
+          total_ages  += info.to_f
+        end
+      end
+end
+  ave =(total_ages/counter)
+  ave.round
 end
